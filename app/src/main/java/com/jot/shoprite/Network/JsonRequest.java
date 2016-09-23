61d760e099de0e45c0a97755a6a9b49b248345ae
Danchab <banchabala@gmail.com>
2016-09-23 04:50:05 +0200

    Added Const class which contains all constants values

--- app/src/main/java/com/jot/shoprite/Network/JsonRequest.java
+++ app/src/main/java/com/jot/shoprite/Network/JsonRequest.java
@@ -1,10 +1,15 @@
 package com.jot.shoprite.Network;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+
+import com.android.volley.Request.Method;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.jot.shoprite.R;
 
 /**
  * Created by D4n on 9/22/2016.
@@ -15,8 +20,8 @@ public class JsonRequest extends Activity implements OnClickListener{
     private String TAG = JsonRequest.class.getSimpleName();
     private TextView mPrice;
     private TextView mItemName;
-    private TextView id;
-
+    private TextView mId;
+    private ProgressDialog pDialog;
 
     //These tags will be used to cancel the requests
     private String tag_json_obj = "jobj_req", tag_json_array = "jarray_req";
@@ -24,7 +29,32 @@ public class JsonRequest extends Activity implements OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mItemName = (TextView) findViewById(R.id.itemPrice);
+        mPrice = (TextView) findViewById(R.id.itemPrice);
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
 
+    }
+
+    private void showProgressDialog(){
+        if (!pDialog.isShowing()){
+            pDialog.show();
+        }
+    }
+
+    private void hideProgressDialog(){
+        if (pDialog.isShowing()){
+            pDialog.hide();
+        }
+    }
+
+    /**
+     * Making json object request
+     * */
+    private void makeJasonObjReq(){
+        showProgressDialog();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,);
     }
 
     @Override
---
+++ app/src/main/java/com/jot/shoprite/Utils/Const.java
@@ -0,0 +1,10 @@
+package com.jot.shoprite.Utils;
+
+/**
+ * Created by D4n on 9/23/2016.
+ */
+
+public class Const {
+    public static final String URL_JSONOBJECT = "";
+    public static final String URL_IMAGE = "";
+}
